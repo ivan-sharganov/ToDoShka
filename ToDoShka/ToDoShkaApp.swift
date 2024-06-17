3689ec5d1495c9de7345afe5a13aa1cb95f7c4ef
Ivan Sharganov <i.sharganov@vk.team>
2024-06-18 01:58:36 +0300

    Initial Commit

---
+++ ToDoShka/ToDoShkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoShkaApp.swift
+//  ToDoShka
+//
+//  Created by i.sharganov on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoShkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
